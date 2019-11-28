e3295ce87a1a26c0f74cdc390cc91d2a03810b18
naijoug <naijoug@126.com>
2019-11-28 22:58:11 +0800

    add sort playground docs

--- swift/Sort.playground/Pages/01 - BubbleSort.xcplaygroundpage/Contents.swift
+++ swift/Sort.playground/Pages/01 - BubbleSort.xcplaygroundpage/Contents.swift
@@ -1,11 +1,14 @@
-//: [Previous](@previous)
+//: [README](@previous)
 
 import Foundation
 
-// S1. 冒泡排序
+//: S1. 冒泡排序
+/*:
+ 冒泡排序的原理是，每一次遍历数组，都去不断地比较相邻的两个元素，如果它们的顺序不对，就交换这两个元素，比如把较大的换到后面。第一次遍 历可以把最大的元素确定下来，放在最后的位置。第二次遍历可以确定第二大的元素，依次类推。这样遍历 N 次后，整个数组就变成递增有序。
+ */
 
 // Time: O(n^2)     Space: O(n)
-func bubbleSort(_ array: [Int]) -> [Int] {
+func sort(_ array: [Int]) -> [Int] {
     var arr = array
     for i in 0..<arr.count {
         for j in 0..<arr.count-i-1 {
@@ -21,6 +24,6 @@ func bubbleSort(_ array: [Int]) -> [Int] {
 
 let array = [1, 3, 9, 7, 6, 4, 2, 8, 5]
 print(array)
-print(bubbleSort(array))
+print(sort(array))
 
-//: [Next](@next)
+//: [S2. 鸡尾酒排序](@next)
